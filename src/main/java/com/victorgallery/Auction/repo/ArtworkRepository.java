6bf0b470899deb7170e6b7a40b82869785dbda68
Gerkep <piotrg2003@gmail.com>
2022-06-17 10:23:41 +0200

    withdrawals and live artworks from ipfs

--- src/main/java/com/victorgallery/Auction/repo/ArtworkRepository.java
+++ src/main/java/com/victorgallery/Auction/repo/ArtworkRepository.java
@@ -1,9 +1,32 @@
 package com.victorgallery.Auction.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
 
 import com.victorgallery.Auction.model.Artwork;
 
-public interface ArtworkRepository extends CrudRepository<Artwork, Long> {
+public interface ArtworkRepository extends Repository<Artwork, Long> {
 
+    <S extends Artwork> S save(S var1);
+
+    <S extends Artwork> Iterable<S> saveAll(Iterable<S> var1);
+
+    Optional<Artwork> findById(Long var1);
+
+    boolean existsById(Long var1);
+
+    Iterable<Artwork> findAll();
+
+    Iterable<Artwork> findAllById(Iterable<Long> var1);
+
+    long count();
+
+    void deleteById(Long var1);
+
+    void delete(Artwork var1);
+
+    void deleteAll(Iterable<? extends Artwork> var1);
+
+    void deleteAll();
 }
--- src/main/java/com/victorgallery/Auction/repo/AuctionRepository.java
+++ src/main/java/com/victorgallery/Auction/repo/AuctionRepository.java
@@ -2,13 +2,32 @@ package com.victorgallery.Auction.repo;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import com.victorgallery.Auction.model.Auction;
 
 
-public interface AuctionRepository extends CrudRepository<Auction, Long> {
+public interface AuctionRepository extends Repository<Auction, Long> {
 
-    void save(Optional<Auction> auction);
+    <S extends Auction> S save(S var1);
 
+    <S extends Auction> Iterable<S> saveAll(Iterable<S> var1);
+
+    Optional<Auction> findById(Long var1);
+
+    boolean existsById(Long var1);
+
+    Iterable<Auction> findAll();
+
+    Iterable<Auction> findAllById(Iterable<Long> var1);
+
+    long count();
+
+    void deleteById(Long var1);
+
+    void delete(Auction var1);
+
+    void deleteAll(Iterable<? extends Auction> var1);
+
+    void deleteAll();
 }
